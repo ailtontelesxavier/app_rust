2d1544688e658e20c07c11f039a44232a3cfde9d
Ailton teles xavier <atxshared@gmail.com>
2025-08-04 07:57:25 -0300

    inicial

---
+++ app/src/main.rs
@@ -0,0 +1,82 @@
+use axum::{
+    Router,
+    extract::{Json, State},
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::{get, post},
+};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use tower_http::trace::TraceLayer;
+use serde::{Deserialize, Serialize};
+use sqlx::{FromRow, PgPool};
+use std::{fmt, sync::Arc};
+use tower_http::services::ServeDir;
+
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+
+use dotenv::dotenv;
+
+#[derive(Clone)]
+struct AppState {
+    db_pool: Arc<PgPool>,
+}
+
+#[derive(Debug)]
+enum AppError {
+    InvalidInput(String),
+    InternalServerError,
+    NotFound,
+}
+
+// Implement IntoResponse for AppError
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        let (status, error_message) = match self {
+            AppError::InvalidInput(msg) => (StatusCode::BAD_REQUEST, msg),
+            AppError::InternalServerError => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Internal server error".to_string(),
+            ),
+            AppError::NotFound => (StatusCode::NOT_FOUND, "Resource Not Found".to_string()),
+        };
+
+        (status, error_message).into_response()
+    }
+}
+
+async fn hello_world() -> &'static str {
+    "Welcome to the Task API with Metrics!"
+}
+
+#[tokio::main]
+async fn main() {
+
+    dotenv().ok();
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let db_pool = PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await
+        .expect("Failed to connect to the database");
+
+    let state = AppState { 
+        db_pool: Arc::new(db_pool) 
+    };
+
+    // Initialize tracing
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new("info,tower_http=debug"))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:2000").await.unwrap();
+
+    let app = Router::new()
+        .route("/", get(hello_world))
+        .layer(TraceLayer::new_for_http())
+        .with_state(state);
+
+    println!("Server running on http://0.0.0.0:2000");
+    axum::serve(listener, app).await.unwrap();
+}
